b4e7fc1236e0456675435afc14e3bea289769908
Harikrishnangithub <Harikrishnangithub@gmail.com>
2019-09-30 09:55:29 +0530

    Changes Made

--- src/test/java/org/test/utils/Test1.java
+++ src/test/java/org/test/utils/Test1.java
@@ -16,8 +16,7 @@ public class Test1 {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\eclipse-workspace\\WaitsConcept\\target\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://adactin.com/HotelApp/");
-		FluentWait f = new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS).pollingEvery(1, TimeUnit.MILLISECONDS).ignoring(Exception.class);
-		WebElement e = (WebElement)f.until(ExpectedConditions.elementToBeClickable(By.id("usernam")));
-		driver.findElement(By.id("usernam")).sendKeys("shahul");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+		
 	}
 }
